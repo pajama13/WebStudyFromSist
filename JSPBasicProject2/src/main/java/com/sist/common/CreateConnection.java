acda7784ce0e80781d13e0924bc4ea077e002b41
pajama13 <1_13_@naver.com>
2023-01-17 22:03:38 +0900

    JSP 기초 개념 필기

---
+++ JSPBasicProject2/src/main/java/com/sist/common/CreateConnection.java
@@ -0,0 +1,34 @@
+package com.sist.common;
+import java.sql.*;
+
+public class CreateConnection {
+	
+	private final String URL="jdbc:oracle:thin:@localhost:1521:XE";
+	public CreateConnection()
+	{
+		try
+		{
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		}catch(Exception ex) {}
+		
+	}
+	public Connection getConnect()
+	{
+		Connection conn=null;
+		try
+		{
+			conn=DriverManager.getConnection(URL,"hr","happy");
+		}catch(Exception ex) {}
+		return conn;
+	}
+	
+	public void disConnection(PreparedStatement ps, Connection conn)
+	{
+		try
+		{
+			if(ps!=null) ps.close();
+			if(conn!=null) conn.close();
+		}catch(Exception ex) {}
+	}
+	 
+}
